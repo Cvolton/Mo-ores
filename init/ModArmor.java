2d6f97e57c318bb8a5e7857bf7fb6ac7c555091e
Cvolton <clubpenguinmisa@gmail.com>
2014-12-26 17:37:16 +0100

    Started adding the copper armor, will finish when I get home

--- init/ModArmor.java
+++ init/ModArmor.java
@@ -13,6 +13,7 @@ public class ModArmor {
 	//      what the heck does that new int[] do? i get it now, its the armor points
 	public static ArmorMaterial ruby = EnumHelper.addArmorMaterial("ruby", "ruby", 18, new int[]{2, 7, 6, 2}, 10);
 	public static ArmorMaterial amethyst = EnumHelper.addArmorMaterial("amethyst", "amethyst", 45, new int[]{4, 8, 7, 4}, 30);
+	public static ArmorMaterial amethyst = EnumHelper.addArmorMaterial("copper", "copper", //dodelat45, new int[]{4, 8, 7, 4}, 30);
 //Rubinovy brneni!
 	public static Item rubyHelmet = new CvoltonArmor(ruby, 0, 0).setUnlocalizedName("rubyHelmet");
 public static Item rubyChestplate = new CvoltonArmor(ruby, 0, 1).setUnlocalizedName("rubyChestplate");
@@ -23,6 +24,11 @@ public static Item amethystHelmet = new CvoltonArmor(amethyst, 0, 0).setUnlocali
 public static Item amethystChestplate = new CvoltonArmor(amethyst, 0, 1).setUnlocalizedName("amethystChestplate");
 public static Item amethystLeggins = new CvoltonArmor(amethyst, 0, 2).setUnlocalizedName("amethystLeggins");
 public static Item amethystBoots = new CvoltonArmor(amethyst, 0, 3).setUnlocalizedName("amethystBoots");
+//Copper
+public static Item copperHelmet = new CvoltonArmor(copper, 0, 0).setUnlocalizedName("copperHelmet");
+public static Item copperChestplate = new CvoltonArmor(copper, 0, 1).setUnlocalizedName("copperChestplate");
+public static Item copperLeggins = new CvoltonArmor(copper, 0, 2).setUnlocalizedName("copperLeggins");
+public static Item copperBoots = new CvoltonArmor(copper, 0, 3).setUnlocalizedName("copperBoots");
 	public static void init(){
 	//Rubin
 		RegisterHelper.registerItem(rubyHelmet);
@@ -34,5 +40,10 @@ public static Item amethystBoots = new CvoltonArmor(amethyst, 0, 3).setUnlocaliz
 		RegisterHelper.registerItem(amethystChestplate);
 		RegisterHelper.registerItem(amethystLeggins);
 		RegisterHelper.registerItem(amethystBoots);
+		//Copper
+		RegisterHelper.registerItem(copperHelmet);
+		RegisterHelper.registerItem(copperChestplate);
+		RegisterHelper.registerItem(copperLeggins);
+		RegisterHelper.registerItem(copperBoots);
 }
 }
